7267a32d25b764fb90403924441bac0e746b5470
hawraayahya <202306317@student.polytechnic.bh>
2025-10-07 15:27:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("contactform").
+onsubmit = function() {
+alert("Your inquiry has been submitted. ");
+};
+document.getElementsByTagName("tr")
+[1].onClick = function(){
+alert("Tutor Info:n\Date:Monday\nTime: 10-11 PM\nLocation:B20.104");
+};
+document.getElementsByTagName("tr") [2].onclick = function() {
+alert("Tutor Info: /nDate: Thursday\nTime:12-1 PM\nLocation: B20.104");
+};
